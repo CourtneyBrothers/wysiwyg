1c325bebb48cc4dde0720ddb641ab4b19df00ece
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-08 15:00:11 -0600

    for every object in array create card

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,37 +1,89 @@
 let arrayOfFamousPeople = [
     {
-        title: "Wizard",
-        name: "Tomoe Gozen",
+        title: "Beyonce",
+        name: "Destiny's Child Beyonce",
         bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
         image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
         lifespan: {
-             birth: 1747,
-            death: 1797
+             birth: 1997,
+            death: 2006
         }
     },
     {
-        title: "Samurai",
-        name: "Tomoe Gozen",
+        title: "Beyonce",
+        name: "Dangerously in love Beyonce",
         bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
         image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
         lifespan: {
-          birth: 1747,
-          death: 1797
+          birth: 2003,
+          death: 2006
         }
     },
         {
-            title: "Samurai",
-            name: "Tomoe Gozen",
+            title: "Beyonce",
+            name: "B-day Beyonce",
             bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
             image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
             lifespan: {
-              birth: 1747,
-              death: 1797
+              birth: 2006,
+              death: 2008
             }
-        }
-    ]
+        },
+        {
+            title: "Beyonce",
+            name: "Sasha Fierce",
+            bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+            image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+            lifespan: {
+              birth: 2008,
+              death: 2011
+            }
+        },
+            {
+                title: "Beyonce",
+                name: "Year of 4 Beyonce",
+                bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+                image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+                lifespan: {
+                  birth: 2011,
+                  death: 2013
+                }
+            },
+            {
+                title: "Beyonce",
+                name: "Beyonce",
+                bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+                image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+                lifespan: {
+                  birth: 2013,
+                  death: 2016
+                }
+            },
+            {
+                title: "Beyonce",
+                name: "Lemonade",
+                bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+                image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+                lifespan: {
+                  birth: 2016,
+                  death: "never"
+                }
+            }
+    ];
 
-let container = document.getElementById("main-div");
+let mainDiv = document.getElementById("main-div");
 let textInput = document.createElement("input");
 textInput.type = "text";
-container.appendChild(textInput);
+mainDiv.appendChild(textInput);
+
+let container = document.getElementById("container");
+
+for (i=0; i < arrayOfFamousPeople.length; i++){
+    let container = document.getElementById("container");
+   card = document.createElement("div");
+   card.id = "card";
+   container.appendChild(card);
+   card.innerHTML = "this card here";
+   console.log("here");
+   };
+
