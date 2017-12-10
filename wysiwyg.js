8cb7703fd7880436e42bca6cd59d2a38265377c4
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-09 20:56:09 -0600

    add images and inner html to card elements

--- wysiwyg.js
+++ wysiwyg.js
@@ -3,7 +3,7 @@ let arrayOfFamousPeople = [
         title: "Beyonce",
         name: "Destiny's Child Beyonce",
         bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        image: "img/destiny.jpg",
         lifespan: {
              birth: 1997,
             death: 2006
@@ -13,7 +13,7 @@ let arrayOfFamousPeople = [
         title: "Beyonce",
         name: "Dangerously in love Beyonce",
         bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        image: "img/dangerousb.jpeg",
         lifespan: {
           birth: 2003,
           death: 2006
@@ -23,7 +23,7 @@ let arrayOfFamousPeople = [
             title: "Beyonce",
             name: "B-day Beyonce",
             bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-            image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+            image: "img/beyonceirrep.jpg",
             lifespan: {
               birth: 2006,
               death: 2008
@@ -33,7 +33,7 @@ let arrayOfFamousPeople = [
             title: "Beyonce",
             name: "Sasha Fierce",
             bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-            image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+            image: "img/singleb.jpg",
             lifespan: {
               birth: 2008,
               death: 2011
@@ -43,7 +43,7 @@ let arrayOfFamousPeople = [
                 title: "Beyonce",
                 name: "Year of 4 Beyonce",
                 bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-                image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+                image: "img/Beyonce-Party.gif",
                 lifespan: {
                   birth: 2011,
                   death: 2013
@@ -53,7 +53,7 @@ let arrayOfFamousPeople = [
                 title: "Beyonce",
                 name: "Beyonce",
                 bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-                image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+                image: "img/beyoncexo.gif",
                 lifespan: {
                   birth: 2013,
                   death: 2016
@@ -63,7 +63,7 @@ let arrayOfFamousPeople = [
                 title: "Beyonce",
                 name: "Lemonade",
                 bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-                image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+                image: "img/lemonadebeyonce.jpg",
                 lifespan: {
                   birth: 2016,
                   death: "never"
@@ -81,10 +81,17 @@ let container = document.getElementById("container");
 for (i=0; i < arrayOfFamousPeople.length; i++){
     let container = document.getElementById("container");
    card = document.createElement("div");
-   card.id = "card";
+   card.classList.add ("card");
+
    container.appendChild(card);
-   card.innerHTML = "this card here";
+   card.innerHTML = `<h1>${arrayOfFamousPeople[i].title}</h1><h2>${arrayOfFamousPeople[i].name}</h2><section>${arrayOfFamousPeople[i].bio}<img src = ${arrayOfFamousPeople[i].image}></section><footer>${arrayOfFamousPeople[i].lifespan.birth}-${arrayOfFamousPeople[i].lifespan.death}</footer>`;
    console.log("here");
-
+   
    };
-
+   
+cards = document.getElementsByClassName("card");
+console.log(cards);
+for (i=0; i < cards.length; i++){
+    console.log(cards.length);
+    card.addEventListener("click", () => card.style.border = "1px dotted black");
+};
